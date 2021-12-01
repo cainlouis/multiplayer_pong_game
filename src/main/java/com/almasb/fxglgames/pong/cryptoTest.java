0d26fb89fd6aef8ddd32d91a2f8dd37784e91e0e
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-12-01 11:40:36 -0500

    test the hash code

--- src/main/java/com/almasb/fxglgames/pong/cryptoTest.java
+++ src/main/java/com/almasb/fxglgames/pong/cryptoTest.java
@@ -1,10 +1,15 @@
 package com.almasb.fxglgames.pong;
 
 import java.security.KeyStoreException;
+import java.security.MessageDigest;
 
 public class cryptoTest {
     public static void main(String[] args) throws Exception {
-        KeyStoring ks = new KeyStoring("test12");
+
+        byte [] byteHash = HashingSHA3.computeHash("test12");
+        String hash = HashingSHA3.bytesToHex(byteHash);
+
+        KeyStoring ks = new KeyStoring(hash);
         ks.createStoredKeys();
         System.out.println(ks.GetPrivateKey().toString());
         System.out.println(ks.GetPublicKey().toString());
