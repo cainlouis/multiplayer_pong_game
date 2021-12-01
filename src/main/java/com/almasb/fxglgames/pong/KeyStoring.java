b81610f532b4e2a4634a6045d7e8837c90e25099
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-11-30 20:10:44 -0500

    add getters for the keys

--- src/main/java/com/almasb/fxglgames/pong/KeyStoring.java
+++ src/main/java/com/almasb/fxglgames/pong/KeyStoring.java
@@ -5,10 +5,9 @@ import javax.crypto.SecretKey;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.KeyStore.*;
+import java.security.cert.Certificate;
 
 public class KeyStoring {
     private KeyStore keyStore;
@@ -24,16 +23,19 @@ public class KeyStoring {
 
     public void createStoredKeys(){
         try {
+            // Create the private and public key using keytool command.
             ProcessBuilder processBuilder = new ProcessBuilder().command(cmdArg);
             processBuilder.start();
-
+            //Create the secret key and store it in the keystore file
+            saveSecretKey();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void LoadKey() throws Exception {
+    private void LoadKey() throws Exception {
+        //loads the p12 file and checks if the hash matches with the keystore's hash.
         FileInputStream fi = new FileInputStream(dir);
         keyStore.load(fi, password.toCharArray());
         if(fi != null){
@@ -41,18 +43,41 @@ public class KeyStoring {
         }
     }
 
-    public void saveSecretKey() throws Exception {
-        SecretKey sk = GenerateKey();
+    private void saveSecretKey() throws Exception {
+        // Load the keystore saved in the keystore/keystore.p12
         LoadKey();
-        keyStore.setKeyEntry(SECRET_KEY, sk, password.toCharArray(), null);
+        // Generating the secret key to store it in the loaded file.
+        SecretKey sk = GenerateKey();
+
+        ProtectionParameter protParam = new PasswordProtection(password.toCharArray());
+        // creates a new entry in the p12 file with the secret key
+        SecretKeyEntry skEntry = new SecretKeyEntry(sk);
+        keyStore.setEntry(SECRET_KEY, skEntry, protParam);
     }
 
-
+//Generates a Secret key using AES Algorithm
     private SecretKey GenerateKey() throws NoSuchAlgorithmException, NoSuchAlgorithmException {
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         keyGen.init(256); //Initialize the key generator
         SecretKey key = keyGen.generateKey(); //Generate the key
         return key;
     }
+
+    public PrivateKey GetPrivateKey() throws Exception {
+        LoadKey();
+        ProtectionParameter protParam = new PasswordProtection(password.toCharArray());
+        PrivateKeyEntry pkEntry = (PrivateKeyEntry) keyStore.getEntry(KEY_PAIR_ALIAS, protParam);
+        PrivateKey myPrivateKey = pkEntry.getPrivateKey();
+        return myPrivateKey;
+    }
+
+    public PublicKey GetPublicKey() throws Exception {
+        LoadKey();
+        Certificate certificate = keyStore.getCertificate(KEY_PAIR_ALIAS);
+        PublicKey publicKey = certificate.getPublicKey();
+        return publicKey;
+    }
+
+
 
 }
