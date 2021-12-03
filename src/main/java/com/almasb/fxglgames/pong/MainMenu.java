783bcbdca76d5b7baad36663b1b4f30ce4e23c47
Nael Louis <nael.louis1002@gmail.com>
2021-12-03 14:31:07 -0500

    Add dialog box for password input

--- src/main/java/com/almasb/fxglgames/pong/MainMenu.java
+++ src/main/java/com/almasb/fxglgames/pong/MainMenu.java
@@ -3,15 +3,21 @@ package com.almasb.fxglgames.pong;
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringBinding;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import kotlin.Pair;
+
+import java.util.Optional;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getUIFactoryService;
 
@@ -27,7 +33,7 @@ public class MainMenu extends FXGLMenu {
         text.setTranslateX(FXGL.getAppWidth() / 2 - 250 / 2);
         text.setTranslateY(FXGL.getAppHeight() / 2 - 300 / 2);
 
-        PongMenuButton newGame = new PongMenuButton("New Game", () -> fireNewGame());
+        PongMenuButton newGame = new PongMenuButton("New Game", () -> loginDialog());
         PongMenuButton exit = new PongMenuButton("Quit", () -> fireExit());
 
         VBox container = new VBox(newGame, exit);
@@ -35,6 +41,39 @@ public class MainMenu extends FXGLMenu {
         container.setTranslateY(450);
 
         getContentRoot().getChildren().addAll(text, container);
+    }
+
+    protected void loginDialog() {
+        // Create the custom dialog.
+        Dialog<String> dialog = new Dialog<>();
+        dialog.setTitle("Login Dialog");
+        dialog.setHeaderText("Password input");
+
+        // Set the button types.
+        ButtonType loginButtonType = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        // Create the password label and field.
+        PasswordField password = new PasswordField();
+        password.setPromptText("Password");
+
+        grid.add(new Label("Password:"), 0, 0);
+        grid.add(password, 1, 0);
+
+        dialog.getDialogPane().setContent(grid);
+
+        // Request focus on the username field by default.
+        Platform.runLater(() -> password.requestFocus());
+
+        Optional<String> result = dialog.showAndWait();
+
+        //Uncomment if password is right
+        //fireNewGame();
     }
 
 }
