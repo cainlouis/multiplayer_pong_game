ff5cd33ea13752afadb7d019531420f7f7459143
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-12-03 18:08:02 -0500

    modify methods to take paths

--- src/main/java/com/almasb/fxglgames/pong/SigningFile.java
+++ src/main/java/com/almasb/fxglgames/pong/SigningFile.java
@@ -1,6 +1,11 @@
 package com.almasb.fxglgames.pong;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.*;
 
 public class SigningFile {
@@ -10,8 +15,8 @@ public class SigningFile {
     /**
      * Method for generating digital signature.
      */
-    public static byte[] generateSignature (PrivateKey privatekey, String message) throws NoSuchAlgorithmException, NoSuchProviderException,
-            InvalidKeyException, UnsupportedEncodingException, SignatureException, UnsupportedEncodingException {
+    public static void generateSignature (PrivateKey privatekey, Path path) throws NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidKeyException, IOException, SignatureException {
 
         //Create an instance of the signature scheme for the given signature algorithm
         Signature sig = Signature.getInstance(algorithm, "SunEC");
@@ -20,19 +25,21 @@ public class SigningFile {
         sig.initSign(privatekey);
 
         //Compute the signature
+        String message = new String(Files.readAllBytes(path));
         sig.update(message.getBytes("UTF-8"));
         byte[] signature = sig.sign();
-
-        return signature;
+        if(Files.notExists(Paths.get("src", "main", "resources","PongApp.sig"))) {
+            Files.write(Paths.get("src", "main", "resources", "PongApp.sig"), signature);
+        }
     }
 
 
     /**
      * Method for verifying digital signature.
      */
-    public static boolean verifySignature(byte[] signature, PublicKey publickey, String message)
+    public static boolean verifySignature(PublicKey publickey, Path path)
             throws NoSuchAlgorithmException, NoSuchProviderException,
-            InvalidKeyException, UnsupportedEncodingException, SignatureException {
+            InvalidKeyException, IOException, SignatureException {
 
         //Create an instance of the signature scheme for the given signature algorithm
         Signature sig = Signature.getInstance(algorithm, "SunEC");
@@ -40,11 +47,12 @@ public class SigningFile {
         //Initialize the signature verification scheme.
         sig.initVerify(publickey);
 
+        String message = new String(Files.readAllBytes(path));
         //Compute the signature.
         sig.update(message.getBytes("UTF-8"));
 
         //Verify the signature.
-        boolean validSignature = sig.verify(signature);
+        boolean validSignature = sig.verify(readSignature());
 
         if(validSignature) {
             System.out.println("\nSignature is valid");
@@ -53,6 +61,12 @@ public class SigningFile {
         }
 
         return validSignature;
+    }
+
+
+    public static byte[] readSignature() throws IOException {
+        return Files.readAllBytes(Paths.get("src", "main", "resources","PongApp.sig",""));
+
     }
 
 }
