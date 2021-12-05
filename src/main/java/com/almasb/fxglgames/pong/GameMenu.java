8fd421708c829e7f68b1fbe8c7fc1aeca0c4964d
Nael Louis <nael.louis1002@gmail.com>
2021-12-05 10:43:27 -0500

    Add comments

--- src/main/java/com/almasb/fxglgames/pong/GameMenu.java
+++ src/main/java/com/almasb/fxglgames/pong/GameMenu.java
@@ -18,27 +18,30 @@ public class GameMenu extends FXGLMenu {
     public GameMenu() {
         super(MenuType.GAME_MENU);
 
-        PongMenuButton resume = new PongMenuButton("Resume", () -> fireResume());
+        //Create the button for the main menu and their action
+        PongMenuButton resume = new PongMenuButton("Resume", () -> fireResume()); //Allow the user to continue the current game
         PongMenuButton save = new PongMenuButton("Save", () -> {
             try {
                 PongApp.saveLastGame();
             } catch (Exception e) {
             }
-        });
+        }); //Allow the user to save the current game
         PongMenuButton load = new PongMenuButton("Load", () -> {
             try {
                 PongApp.loadLastGame();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        });
-        PongMenuButton mainMenu = new PongMenuButton("Main Menu", () -> fireExitToMainMenu());
-        PongMenuButton exit = new PongMenuButton("Exit Game", () -> fireExit());
+        }); //Allow the user to load the previous game
+        PongMenuButton mainMenu = new PongMenuButton("Main Menu", () -> fireExitToMainMenu()); //Allow the user to return to the main menu
+        PongMenuButton exit = new PongMenuButton("Exit Game", () -> fireExit()); //exit the application
 
+        //Create a vbox that acts like a container for the buttons and set the position to the lower left corner
         VBox container = new VBox(resume, save, load, mainMenu, exit);
         container.setTranslateX(100);
         container.setTranslateY(450);
 
+        //Add the container to the scene
         getContentRoot().getChildren().add(container);
     }
 
