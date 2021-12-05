d6a32ef8d49b4e85ffe939af7534143d9d49c309
Nael Louis <nael.louis1002@gmail.com>
2021-12-05 10:43:02 -0500

    add load option and comments

--- src/main/java/com/almasb/fxglgames/pong/MainMenu.java
+++ src/main/java/com/almasb/fxglgames/pong/MainMenu.java
@@ -35,17 +35,27 @@ public class MainMenu extends FXGLMenu {
     public MainMenu() {
         super(MenuType.MAIN_MENU);
 
+        //Create text for the name of the game
         Text text = getUIFactoryService().newText("Pong Game", Color.BLACK, 60.0);
         text.setTranslateX(FXGL.getAppWidth() / 2 - 250 / 2);
         text.setTranslateY(FXGL.getAppHeight() / 2 - 300 / 2);
 
-        PongMenuButton newGame = new PongMenuButton("New Game", () -> fireNewGame());
-        PongMenuButton exit = new PongMenuButton("Quit", () -> fireExit());
+        //Create the button for the main menu and their action
+        PongMenuButton newGame = new PongMenuButton("New Game", () -> fireNewGame()); //Create a new instance of the game
+        PongMenuButton exit = new PongMenuButton("Quit", () -> fireExit()); //Exit the application
+        PongMenuButton load = new PongMenuButton("Load Game", () -> {
+            try {
+            PongApp.loadLastGame();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }}); //Allow the user to load a previous game
 
-        VBox container = new VBox(newGame, exit);
+        //Create a vbox that acts like a container for the buttons and set the position to the lower left corner
+        VBox container = new VBox(newGame, load, exit);
         container.setTranslateX(100);
         container.setTranslateY(450);
 
+        //Add the container and the text for the game to the scene
         getContentRoot().getChildren().addAll(text, container);
     }
 }
